0120fbd9c7468b94044bf5eb90a92e3aceec30d5
Service <info@service.money>
2026-01-14 20:28:09 -0500

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,17 @@
+export const Utils = {
+    formatCurrency: (num) => {
+        return new Intl.NumberFormat('en-US', { 
+            style: 'currency', 
+            currency: 'USD' 
+        }).format(num);
+    },
+
+    parseDate: (str) => {
+        const date = new Date(str);
+        return isNaN(date) ? null : date;
+    },
+
+    generateId: (prefix = 'id') => {
+        return `${prefix}-${Date.now()}-${Math.floor(Math.random()*1000)}`;
+    }
+};
